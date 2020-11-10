8bab6a736f7f5534d51b47d2223b54e07d8e817d
Dayse Soares <daysedesign@gmail.com>
2020-11-10 20:43:55 -0300

    trabalhando com metaTags openGraph e TwitterCard

--- src/main/java/br/com/daysesoares/ajax/DemoSpringMvcAjaxApplication.java
+++ src/main/java/br/com/daysesoares/ajax/DemoSpringMvcAjaxApplication.java
@@ -1,13 +1,29 @@
 package br.com.daysesoares.ajax;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.daysesoares.ajax.domain.SocialMetaTag;
+import br.com.daysesoares.ajax.services.SocialMetaTagService;
+
 @SpringBootApplication
-public class DemoSpringMvcAjaxApplication {
+public class DemoSpringMvcAjaxApplication implements CommandLineRunner{
+	
+	@Autowired
+	private SocialMetaTagService service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoSpringMvcAjaxApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		SocialMetaTag smt = service.getSocialMetaTagByUrl("https://www.udemy.com/course/spring-boot-mvc-com-thymeleaf/");
+		System.out.println(smt.toString());
+		
+		
 	}
 
 }
---
+++ src/main/java/br/com/daysesoares/ajax/domain/SocialMetaTag.java
@@ -0,0 +1,14 @@
+package br.com.daysesoares.ajax.domain;
+
+import lombok.Data;
+
+@Data
+public class SocialMetaTag {
+
+	private String site;
+	private String title;
+	private String url;
+	private String image;
+	
+	
+}
