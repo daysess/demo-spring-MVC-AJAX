99c416814aefaed4d82079fb7f2ec459b6701635
Dayse Soares <daysedesign@gmail.com>
2020-11-10 17:16:27 -0300

    Inicio do projeto

---
+++ src/main/java/br/com/daysesoares/ajax/DemoSpringMvcAjaxApplication.java
@@ -0,0 +1,13 @@
+package br.com.daysesoares.ajax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringMvcAjaxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringMvcAjaxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/daysesoares/ajax/DemoSpringMvcAjaxApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.daysesoares.ajax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringMvcAjaxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
