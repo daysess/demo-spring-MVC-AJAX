aa8166f640cf89730a4eb988be80f23e08d23fde
Dayse Soares <daysedesign@gmail.com>
2020-11-17 21:22:50 -0300

    Lidando com as curtidas e filtrando promocoes

--- src/main/resources/static/js/promo-list.js
+++ src/main/resources/static/js/promo-list.js
@@ -22,11 +22,13 @@ $(window).scroll(function(){
 });
 
 function loadByScrollBar(pageNumber){
+	var site = $("#autocomplete-input").val();
 	$.ajax({
 		method: "GET",
 		url: "/promocao/list/ajax",
 		data: {
-			page: pageNumber
+			page: pageNumber,
+			site: site
 		},
 		beforeSend: function(){
 			$("#loader-img").show();
@@ -47,10 +49,76 @@ function loadByScrollBar(pageNumber){
 
 		},
 		error: function(xhr){
-			alert("Ops, ocorreu um erro: "+xhr.status + " - " + xhr.statusText);
+			alert("Ops, ocorreu um erro: "
+					+ xhr.status 
+					+ " - " 
+					+ xhr.statusText);
 		},
 		complete: function(){
 			$("#loader-img").hide();
 		}
 	});
 }
+
+//adicionar likes
+$(document).on("click", "button[id*='likes-btn-']", function(){
+	var id = $(this).attr("id").split("-")[2];
+	console.log("id: ", id);
+	
+	$.ajax({
+		method: "POST",
+		url: "/promocao/like/" + id,
+		success: function(response){
+			$("#likes-count-" + id).text(response);
+		},
+		error: function(xhr){
+			alert("Ops, ocorreu um erro: "+ xhr.status + ", " + xhr.statusText);
+		}
+	})
+});
+
+
+// autocomplete
+$("#autocomplete-input").autocomplete({
+	source: function(request, response){
+		$.ajax({
+			method: "GET",
+			url: "/promocao/site",
+			data: {
+				termo: request.term
+			},
+			success: function(result){
+				response(result);
+			}
+		});
+	}
+});
+
+$("#autocomplete-submit").on("click", function(){
+	var site = $("#autocomplete-input").val();
+	$.ajax({
+		method: "GET",
+		url: "/promocao/site/list",
+		data: {
+			site: site
+		},
+		beforeSend: function(){
+			pageNumber = 0;
+			$("#fim-btn").hide();
+			$(".row").fadeOut(400, function(){
+				$(this).empty();
+			});
+		},
+		success: function(response){
+			$(".row").fadeIn(250, function(){
+				$(this).append(response);
+			});
+		},
+		error: function(xhr){
+			alert("Ops, algo deu errado " + xhr.status + ", " + xhr.statusText);
+		}
+	});
+});
+
+
+
